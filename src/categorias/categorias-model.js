29be02516d52c3bd0d0af69a118793f60fdf89d3
JoaquinFigueroa5 <hfigueroa-2023417@kinal.edu.gt>
2025-02-24 16:45:08 -0600

    Modelo de categorias hecho

---
+++ src/categorias/categorias-model.js
@@ -0,0 +1,16 @@
+import {Schema, model} from 'mongoose'
+
+const CategoriaSchema = Schema({
+    nombre: {
+        type: String,
+        required: [true, 'El nombre es obligatorio'],
+        unique: true,
+        maxLenght: [50, 'No puede sobrepasar los 50 caracteres']
+    },
+    descripcion: {
+        type: String,
+        maxLenght: [200, 'No puede sobrepasar los 200 caracteres']
+    }
+})
+
+export default model('Categoria', CategoriaSchema)
